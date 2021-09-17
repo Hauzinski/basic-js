d6033feaa7c8354076bfe2e7d352282b9dbf79bb
Vitali Hauzinski <hauzinski@gmail.com>
2021-09-17 10:16:59 +0300

    st-delete-digit

--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -11,7 +11,12 @@ import { NotImplementedError } from '../extensions/index.js';
  * For n = 152, the output should be 52
  *
  */
-export default function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
+export default function deleteDigit(n) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let arr = String(n).split("").map(Number);
+  let minNumber = Math.min(...arr);
+  let indexMin = arr.findIndex((value)=> value === minNumber);
+  arr.splice(indexMin, 1);
+  return +arr.join("");
 }
