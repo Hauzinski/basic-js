65c57ed7aef560ae24f21662873df5189a805e75
Vitali Hauzinski <hauzinski@gmail.com>
2021-09-17 14:26:47 +0300

    feat: st-sort-by-height

--- src/st-sort-by-height.js
+++ src/st-sort-by-height.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,7 +11,13 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-export default function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+export default function sortByHeight(arr) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let positions = arr.map((value, inndex) => (value === -1 ? inndex : null)).filter((value) => value !== null);
+  let newArray = arr.filter((value) => value !== -1).sort((a, b) => a - b);
+  for (let i = 0; i < positions.length; i++) {
+    newArray.splice(positions[i], 0, -1);
+  }
+  return newArray;
 }
